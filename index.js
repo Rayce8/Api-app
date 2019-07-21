c5433ad2c983c537425cb4255bb6f5574ce2b63b
Rayce8 <50722060+Rayce8@users.noreply.github.com>
2019-07-21 12:45:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const GAME_UNIQUE_KEY_ATTRIBUTE = 'data-game-name';
 
 const apiKey = 'AIzaSyAdDw2g3-gjyBqGs1tdxIUhS3S5V_T5gDo'
 
-/*`
-
-/*https://developers.google.com/youtube/v3/docs/search/list*/
 
 
 
-/*https://rawg.io/apidocs*/
+
+
+
+
 
 function findVids (resultsName) {
   const vidURL = `https://www.googleapis.com/youtube/v3/search/?part=snippet&q=${resultsName}&key=AIzaSyAdDw2g3-gjyBqGs1tdxIUhS3S5V_T5gDo&maxResults=3`
@@ -27,7 +27,7 @@ function findVids (resultsName) {
 }
 
 function displayYoutubeVid (responseJson, gameName) {
-console.log(responseJson);
+
 for (let i = 0; i < responseJson.items.length; i++){
 $(`*[${GAME_UNIQUE_KEY_ATTRIBUTE}="${gameName}"]`).append(
   `<a href='https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}'><img src='${responseJson.items[i].snippet.thumbnails.medium.url}' class = 'youtubeThumbnail'></a>`
@@ -36,7 +36,7 @@ $(`*[${GAME_UNIQUE_KEY_ATTRIBUTE}="${gameName}"]`).append(
 }
 
 function displayResults(responseJson) {
-  console.log(responseJson);
+  
  $('#results').removeClass('hidden');
   $('#gamesList').empty();
  for (let i = 0; i < responseJson.results.length; i++) { 
@@ -83,11 +83,11 @@ $('form').submit(event => {
   let gameInput1 = $('#searchBar').val().trim();
     let gameInput = gameInput1.replace(/\s+/g, '-')
 
-  // if not undefined, and not a zero length string
+  
   if( gameInput ){
     findRecommendations (gameInput)
   } else {
-    // something was wrong with input
+    
     displayErrorMessage("No search game title provided.");
   }
 
